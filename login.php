875850b3a19d92afbaa8d20191030cbc137df939
laoxiao <18476969365@163.com>
2019-06-17 00:06:29 +0800

    7777

--- login.php
+++ login.php
@@ -6,19 +6,38 @@ include "connectSql.php";    //引入连接数据库文件
 //实例化response类
 $response = new Response;
 $connectSql = new ConnectSql;
-$sql = "select * from user where username='张三'";
 
-$row = $connectSql -> querySqlResult("test",$sql); //传入数据库名字和sql语句
+class RequestList{
+    public function login($username,$password){
+        $userName = isset($username?$username:"";
+        $password = isset($password?$password:"";
 
-    if(!($row=='')){
-        //准备返回数据
-        $code = 20000;
-        $message = "信息请求成功";
-        $data = array(
-                "name" => "你爸爸",
-                "list"=>$row
+        if(empty($userName) || empty($password)){
+            $result = array(
+                "code" => "2004",
+                "message" => "参数不能为空",
+                "status" => "no"
             );
-        
-        //返回数据
-        echo $response -> json($code,$message,$data);
-    };
+            echo json_encode($result,320);//JSON_UNESCAPED_UNICODE或者数字都行
+        }else{
+            $sql = "select * from user where username='$userName' && password='$password'";
+            $row = $connectSql -> querySqlResult("test",$sql); //传入数据库名字和sql语句
+            if(!($row=='')){
+                //准备返回数据
+                $code = 20000;
+                $message = "信息请求成功";
+                $data = array(
+                        "name" => "你爸爸",
+                        "list"=>$row
+                    );
+                
+                //返回数据
+                echo $response -> json($code,$message,$data);
+            };
+        }
+    }
+
+}
+
+
+

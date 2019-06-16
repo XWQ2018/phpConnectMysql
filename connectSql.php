b6d8d5c21bf8dcb033f33dd0c85c7edf59694971
laoxiao <18476969365@163.com>
2019-06-16 23:47:17 +0800

    新增登入接口

--- connectSql.php
+++ connectSql.php
@@ -11,21 +11,30 @@ class ConnectSql{
         if($cn->connect_error){
             die("连接失败: " . $cn->connect_error);
         }
-
         $result= $cn->query($sql);
-        // 得到所有结果
-        $row=($result->fetch_all(MYSQLI_ASSOC));
-        // $row=mysqli_fetch_all($result,MYSQLI_ASSOC); //上下两种写法都可以
 
-        return $row;
+        if (mysqli_num_rows($result) < 1){
+            $result = array(
+                "code" => '20000',
+                "message" => "查询无数据！",
+                "data" => array()
+            );
+            echo json_encode($result,320);//JSON_UNESCAPED_UNICODE或者数字都行
+            return '';
+        }else{
+            // 得到所有结果
+            $row=($result->fetch_all(MYSQLI_ASSOC));
+            // $row=mysqli_fetch_all($result,MYSQLI_ASSOC); //上下两种写法都可以
 
-        // 释放结果集 
-        $cn->close();
-        // mysqli_free_result($result); 
-
-        //关闭连接
-        $cn->close();
-        // mysqli_close($con); 
+            return $row;
+            // 释放结果集 
+            $cn->close();
+            // mysqli_free_result($result); 
+    
+            //关闭连接
+            $cn->close();
+            // mysqli_close($con); 
+        }
     }
 
 }

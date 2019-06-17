ccfe1e76a91e708b33833f75ed5b5b901da43c41
XWQ2018 <18476969365@163.com>
2019-06-17 15:23:15 +0800

    jytjrjhr

--- response.class.php
+++ response.class.php
@@ -1,10 +1,4 @@
 <?php
-header('Content-Type: text/html;charset=utf-8');
-header('Access-Control-Allow-Origin:*');// *代表允许任何网址请求
-header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE');// 允许请求的类型
-header('Access-Control-Allow-Credentials: true'); // 设置是否允许发送 cookies
-header('Access-Control-Allow-Headers: Content-Type,Content-Length,Accept-Encoding,X-Requested-with, Origin'); // 设置允许自定义请求头的字段
-
 /**
  *description 用于返回指定数据格式的类
  *@param $code [int] 返回的状态码
